0e703ffe3a1bd0726271107d5d2292224fdcb85f
Cedric Santiago <borikipr@gmail.com>
2026-08-03 21:56:19 -0400

    fix(i18n): correct language selector flags

--- components/LanguageSelector.tsx
+++ components/LanguageSelector.tsx
@@ -22,13 +22,13 @@ const options: ReadonlyArray<{
 }> = [
   {
     locale: DEFAULT_LOCALE,
-    flagSrc: "/flags/puerto-rico.svg",
+    flagSrc: "/flags/puerto-rico1.svg",
     flagWidth: 900,
     flagHeight: 600,
   },
   {
     locale: ENGLISH_LOCALE,
-    flagSrc: "/flags/united-states.svg",
+    flagSrc: "/flags/united-states1.svg",
     flagWidth: 1235,
     flagHeight: 650,
   },
